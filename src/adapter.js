d26816f04333cc0b982e815414fd1abf383ca977
Stephen Hand <steve2727@hotmail.com>
2014-06-05 23:08:29 +0100

    Removed local duplicate, now dogfoods using github

--- src/adapter.js
+++ src/adapter.js
@@ -2,6 +2,14 @@
  * Created by stephen.hand on 04/06/2014.
  */
 (function(window) {
-	window.JsHamcrest.Integration.copyMembers(window);
-	window.JsMockito.Integration.importTo(window);
+	JsHamcrest.Integration.copyMembers(window);
+	JsMockito.Integration.importTo(window);
+	window.assertThat = function(actualValue, matcherOrValue, message) {
+		JsHamcrest.Operators.assert(actualValue, matcherOrValue, {
+			message: message,
+			fail: function (msg) {
+				throw new Error(msg);
+			}
+		});
+	}
 })(window);
