fdd3bc6c012047d0f01039e207921b2d73195658
stephenhand <steve2727@hotmail.com>
2014-06-06 12:45:05 +0100

    Updated bodge to at least attempt to work in cases where global 'require' is in use (by requirejs, for instance)

--- src/fix_jsmockito_npm.js
+++ src/fix_jsmockito_npm.js
@@ -1,5 +1,12 @@
-if (typeof require === 'undefined'){
-	require=function(){
-		return window;
-	}
-}
+(function(){
+    var __require = window.require;
+    window.require = function(){
+        if (arguments[0]==="jshamcrest" && arguments.length === 1){
+            window.require = __require;
+            return window;
+        }
+        else if (typeof __require !== 'undefined'){
+            __require.apply(this, arguments);
+        };
+    };
+})();
